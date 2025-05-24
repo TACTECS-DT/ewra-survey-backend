bd2704be164a6658fa9898f32b60ff04432044ff
TACTECS-DT <117572328+TACTECS-DT@users.noreply.github.com>
2025-05-24 21:39:29 +0300

    remove

--- survey_get/extract_rar_folder.py
+++
@@ -1,9 +0,0 @@
-import rarfile
-
-# Set unrar path manually if needed
-rarfile.UNRAR_TOOL = "/usr/bin/unrar"  # Replace 'yourusername' with your PythonAnywhere username
-
-# Open the RAR file
-rar = rarfile.RarFile("survey.rar")
-rar.extractall("survey_output_folder")  # Extract to 'output_folder'
-print("Extraction complete!")
